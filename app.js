9e4c02603bc446aee7b44c7b09a6134accd22a45
Collin OConnell <ococncol@gmail.com>
2017-03-06 14:00:05 -0500

    remade module

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
+var superHeroList = [
+  {
+    name: "Deadpool",
+    number: "800-KIL-LHIM",
+    group: "Marvel",
+    img: "img/Deadpool-PNG-Photo.png"
+  },
+  {
+    name: "Wolverine",
+    number: "800-HEY-BUBB",
+    group: "Marvel",
+    img: "img/WOLVERINE.png"
+  },
+  {
+    name: "Batman",
+    number: "800-DRK-NITE",
+    group: "DC",
+    img: "img/batman.png"
+  },
+  {
+    name: "Deathstroke",
+    number: "800-HIR-EGUN",
+    group: "DC",
+    img: "img/Deathstroke.png"
+  },
+  {
+    name: "Star Lord(Peter Quill)",
+    number: "800-STA-RLRD",
+    group: "Marvel",
+    img: "img/Starlord.png"
+  }
+];
 
 
-
-angular
-      .module("superHeroPhoneApp", [])
-      ;
+angular.module("superApp", [])
